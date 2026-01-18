90db6a671ba065fbcb16de3448155a4329639905
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2026-01-18 15:30:06 +0530

    routes set up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Cart ,Checkout ,Products ,SingleProduct , Error ,HomeLayout , Login ,Register , Landing } from './pages'
+import { Cart ,Checkout ,Products ,SingleProduct , Error ,HomeLayout , Login ,Register , Landing ,About ,Orders} from './pages'
 import {createBrowserRouter , RouterProvider} from 'react-router-dom';
 
 
@@ -8,17 +8,52 @@ import {createBrowserRouter , RouterProvider} from 'react-router-dom';
 
 const router = createBrowserRouter([
  {
-  path :'/Home',
+  path :'/',
   element:<HomeLayout/>,
-  errorElement : <Error/>
+  errorElement : <Error/>,
+  children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+      { path: 'about', element: <About /> },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+      },
+    ]
+ },
+ {
+  path:'/Login',
+  element:<Login/>,
+  errorElement:<Error/>
+ },
+ {
+  
+  path:'/Register',
+  element:<Register/>,
+  errorElement:<Error/>
+ 
+
  }
 
 ]);
-
-
-
-
-
 
 const App = () => {
   return<RouterProvider router={router}></RouterProvider>
