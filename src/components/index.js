9818dcadd9eb00618cbac3cef2c94d2e6ad0e90b
lalit kashyap <lalit.kashyap.ug20@nsut.ac.in>
2026-01-22 23:02:21 +0530

    feat : nav added

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export {default as FormInput} from './FormInput';
-
+export {default as Header} from './Header';
 export {default as SubmitBtn} from './SubmitBtn';
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,13 +1,17 @@
 
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Header } from '../components'
+import Navbar from '../components/Navbar'
 
 const HomeLayout = () => {
   return (
      <>
+     <Header/>
       <nav>
         <span className='text-4xl text-primary'>Comfy</span>
       </nav>
+      <Navbar/>
       <section className='align-element py-20'>
           <Outlet />
       </section>
